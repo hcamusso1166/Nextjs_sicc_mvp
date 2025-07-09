db13eb902d26576a628efd8df06ab284a0967067
unknown <hcamusso@gmail.com>
2025-07-09 10:57:36 -0300

    integralView con componnetes

--- app/ui/integralView/status.tsx
+++ app/ui/integralView/status.tsx
@@ -8,32 +8,32 @@ export default function DocumentoStatus({ status }: { status?: string }) {
       className={clsx(
         'inline-flex items-center rounded-full px-2 py-1 text-xs',
         {
-          'bg-orange-500 text-white': status === 'published',
-          'bg-yellow-500 text-white': status === 'draft',
-          'bg-red-500 text-white': status === 'archived',
+          'bg-orange-500 text-white': status === 'presented',
+          'bg-yellow-500 text-white': status === 'toPresent',
+          'bg-red-500 text-white': status === 'finalized',
           'bg-green-500 text-white': status === 'approved',
         },
       )}
     >
-      {status === 'published' && (
+      {status === 'approved' && (
         <>
           Published
           <DocumentIcon className="ml-1 w-4 text-white" />
         </>
       )}
-      {status === 'draft' && (
+      {status === 'toPresent' && (
         <>
           Draft
           <ClockIcon className="ml-1 w-4 text-white" />
         </>
       )}
-      {status === 'archived' && (
+      {status === 'finalized' && (
         <>
           Archived
           <XCircleIcon className="ml-1 w-4 text-white" />
         </>
       )}
-      {status === 'approved' && (
+      {status === 'presented' && (
         <>
           Approved
           <CheckIcon className="ml-1 w-4 text-white" />
